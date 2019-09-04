ef968a70b92d9fba5acecea7c82fb9b4ca399854
Marcos Augusto <augustoprofemp@gmail.com>
2019-09-04 19:54:13 -0300

    [FEATURE] Adding axios cals to application
    - Making http calls to github
    - Adding animation
    - Adding styles to disabled button
    - Handling with button when making requests

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,72 @@
-import React from 'react';
-import { Title } from './style';
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import api from '../../services/api';
+import { Container, Form, SubmitButton } from './style';
 
-function Main() {
-  return <Title>Main</Title>;
+// import { Container } from './styles';
+
+export default class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newRepo: '',
+      repositories: [],
+      loading: false,
+    };
+  }
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({
+      loading: true,
+    });
+
+    const { newRepo, repositories } = this.state;
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    });
+  };
+
+  render() {
+    const { newRepo, loading } = this.state;
+    return (
+      <Container>
+        <h1>
+          {' '}
+          <FaGithubAlt />
+          Repositories
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Add Repo"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#FFF" />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
-
-export default Main;
